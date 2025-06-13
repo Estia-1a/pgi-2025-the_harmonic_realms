46cf8875818fad0eb36b0cc243730901502a1238
Mayrickk-NC <aymerick.monin@etu.estia.fr>
2025-06-13 14:55:44 +0200

    appel de min component dans main

--- src/main.c
+++ src/main.c
@@ -38,7 +38,10 @@ int main(int argc, char **argv) {
     /* first_pixel() function is defined in feature.h and implemented in feature.c */
     tenth_pixel(configuration.filenames[0]);
   }
-  
+  if (strncmp(configuration.command, "min_component", 13) == 0) {
+    min_component(configuration.filenames[0], configuration.arguments[0][0]);
+}
+
 
   return 0;
 
